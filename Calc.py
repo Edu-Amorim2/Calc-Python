4bc7f716ff5f48069fb81b7dd9f68aabf89ac770
Eduardo <eduardo.ramos2005@hotmail.com>
2023-05-11 09:48:24 -0300

    first commit

---
+++ Calc.py
@@ -0,0 +1,46 @@
+#Calculadora de Valores Inteiros - @Edu-Amorim2
+print("Calculadora Básica")
+
+def Add():
+    print("Escolhido Adição")
+    valor1 = int(input("Coloque o Primeiro Valor:"))
+    valor2 = int(input("coloque o Segundo Valor:"))
+    result = print("O Resultado é igual à" , valor1 + valor2)
+    
+def Subs():
+    print("Escolhido Substração")
+    valor1 = int(input("Coloque o Primeiro Valor: "))
+    valor2 = int(input("coloque o Segundo Valor:"))
+    result = print("O Resultado é igual à" , valor1 - valor2)
+    
+def Mult():
+    print("Escolhido Multiplicação")
+    valor1 = int(input("Coloque o Primeiro Valor: "))
+    valor2 = int(input("coloque o Segundo Valor:"))
+    result = print("O Resultado é igual à" , valor1 * valor2)
+
+def Divs():
+    print("Escolhido Divisão")
+    valor1 = int(input("Coloque o Primeiro Valor: "))
+    valor2 = int(input("coloque o Segundo Valor:"))
+    result = print("O Resultado é igual à" , valor1 / valor2)
+    
+    
+    
+print("Escolha o Tipo de Conta: \n 1 - Adição \n 2 - Substração \n 3 - Multiplicação \n 4 - Divisão")
+
+
+
+a = int(input("Digite Aqui:"))
+
+if a == 1:
+    Add()
+    
+if a == 2:
+    Subs()
+    
+if a == 3:
+    Mult()
+    
+if a == 4:
+    Divs()
