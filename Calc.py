f341cef87de90b39b035dc64b3e0d743227ec43b
Eduardo Amorim <130883583+Edu-Amorim2@users.noreply.github.com>
2023-05-15 16:03:12 +0000

    Second Commit

--- Calc.py
+++ Calc.py
@@ -1,46 +1,53 @@
 #Calculadora de Valores Inteiros - @Edu-Amorim2
-print("Calculadora Básica")
+while True:
+    print("Calculadora Básica")
 
-def Add():
-    print("Escolhido Adição")
-    valor1 = int(input("Coloque o Primeiro Valor:"))
-    valor2 = int(input("coloque o Segundo Valor:"))
-    result = print("O Resultado é igual à" , valor1 + valor2)
-    
-def Subs():
-    print("Escolhido Substração")
-    valor1 = int(input("Coloque o Primeiro Valor: "))
-    valor2 = int(input("coloque o Segundo Valor:"))
-    result = print("O Resultado é igual à" , valor1 - valor2)
-    
-def Mult():
-    print("Escolhido Multiplicação")
-    valor1 = int(input("Coloque o Primeiro Valor: "))
-    valor2 = int(input("coloque o Segundo Valor:"))
-    result = print("O Resultado é igual à" , valor1 * valor2)
+    def Add():
+        print("Escolhido Adição")
+        valor1 = int(input("Coloque o Primeiro Valor:"))
+        valor2 = int(input("coloque o Segundo Valor:"))
+        result = print("O Resultado é igual à" , valor1 + valor2)
+      
+    def Subs():
+        print("Escolhido Substração")
+        valor1 = int(input("Coloque o Primeiro Valor: "))
+        valor2 = int(input("coloque o Segundo Valor:"))
+        result = print("O Resultado é igual à" , valor1 - valor2)
+        
+    def Mult():
+        print("Escolhido Multiplicação")
+        valor1 = int(input("Coloque o Primeiro Valor: "))
+        valor2 = int(input("coloque o Segundo Valor:"))
+        result = print("O Resultado é igual à" , valor1 * valor2)
 
-def Divs():
-    print("Escolhido Divisão")
-    valor1 = int(input("Coloque o Primeiro Valor: "))
-    valor2 = int(input("coloque o Segundo Valor:"))
-    result = print("O Resultado é igual à" , valor1 / valor2)
-    
-    
-    
-print("Escolha o Tipo de Conta: \n 1 - Adição \n 2 - Substração \n 3 - Multiplicação \n 4 - Divisão")
+    def Divs():
+        print("Escolhido Divisão")
+        valor1 = int(input("Coloque o Primeiro Valor: "))
+        valor2 = int(input("coloque o Segundo Valor:"))
+        result = print("O Resultado é igual à" , valor1 / valor2)
+
+    def Err():
+        print("Programa Encerrado")
+        
+        
+        
+    print("Escolha o Tipo de Conta: \n 1 - Adição \n 2 - Substração \n 3 - Multiplicação \n 4 - Divisão\n 5- Fim")
 
 
+    a = int(input("Digite Aqui:"))
 
-a = int(input("Digite Aqui:"))
-
-if a == 1:
-    Add()
+    if a == 1:
+        Add()
+        
+    if a == 2:
+        Subs()
+        
+    if a == 3:
+        Mult()
+        
+    if a == 4:
+        Divs()
     
-if a == 2:
-    Subs()
-    
-if a == 3:
-    Mult()
-    
-if a == 4:
-    Divs()
+    if a == 5:
+        Err()
+        break
